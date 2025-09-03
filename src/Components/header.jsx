ab9239fab5db075e675f75c17090211415148b4c
nmath-ist <nmath.geral@gmail.com>
2025-09-03 09:03:34 +0100

    Change Image Path

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -7,7 +7,7 @@ export default function Header(props) {
 
   return (
   <header className ='header' >
-  <img className = 'image' src="/Imagens/NMATH.png" alt="NMATH" />
+  <img className = 'image' src="./NMATH.png" alt="NMATH" />
       <h1 className = 'titulo' onClick={props.clickFunction}> Repositório de Matemática </h1>
     <form onSubmit = {props.actionFunction}>
         <input className = 'pesquisar' type="text" placeholder="&#128270; Pesquisar Conteúdo..." name = "query" />
